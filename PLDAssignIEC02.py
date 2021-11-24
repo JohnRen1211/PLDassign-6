293ecd73f942700ecff8b6fe5c33abf7db5c89f7
JohnRen1211 <johnrengelojaylo@iskolarngbayan.pup.edu.ph>
2021-11-25 06:35:32 +0800

    Program completely running

--- PLDAssignIEC02.py
+++ PLDAssignIEC02.py
@@ -113,3 +113,225 @@ elif answer == command1:
 elif answer >= command2:
     print("Input the right command.")
     raise SystemExit
+
+# Program running   
+print("Set#1 of random numbers:")
+num1 = ran_num1 + ran_num2 
+print("The answer in operation#1 is:",num1)
+print("Random number1 is: ",ran_num1)
+print("Random number2 is: ",ran_num2)
+print("Evaluating if the answer is correct: Type 1 for yes and 2 for no command.")
+print("Operation: 1/10")
+answer= int(input("Response: "))
+command1 = 1
+command2 = 2
+if answer == command2:
+    print("Okay, then rest.")
+    raise SystemExit
+elif answer <= -1:
+    print("Input the right command.")
+    raise SystemExit
+elif answer == command1:
+    print("Continue to the survey")
+elif answer >= command2:
+    print("Input the right command.")
+    raise SystemExit
+
+print("Set#2 of random numbers")
+num2 = ran_num3 + ran_num4 
+print("The answer in operation#2 is:",num2)
+print("Random number1 is: ",ran_num3)
+print("Random number2 is: ",ran_num4)
+print("Evaluating if the answer is correct: Type 1 for yes and 2 for no command.")
+print("Operation number: 2/10")
+answer= int(input("Response: "))
+command1 = 1
+command2 = 2
+if answer == command2:
+    print("Okay, then rest.")
+    raise SystemExit
+elif answer <= -1:
+    print("Input the right command.")
+    raise SystemExit
+elif answer == command1:
+    print("Continue to the survey")
+elif answer >= command2:
+    print("Input the right command.")
+    raise SystemExit
+
+print("Set#3 of random numbers")
+num3 = ran_num5 + ran_num6 
+print("The answer in operation#3 is:",num3)
+print("Random number1 is: ",ran_num5)
+print("Random number2 is: ",ran_num6)
+print("Evaluating if the answer is correct: Type 1 for yes and 2 for no command.")
+print("Operation: 3/10")
+answer= int(input("Response: "))
+command1 = 1
+command2 = 2
+if answer == command2:
+    print("Okay, then rest.")
+    raise SystemExit
+elif answer <= -1:
+    print("Input the right command.")
+    raise SystemExit
+elif answer == command1:
+    print("Continue to the survey")
+elif answer >= command2:
+    print("Input the right command.")
+    raise SystemExit
+
+print("Set#4 of random numbers")
+num4 = ran_num7 + ran_num8 
+print("The answer in operation#4 is:",num4)
+print("Random number1 is: ",ran_num7)
+print("Random number2 is: ",ran_num8)
+print("Evaluating if the answer is correct: Type 1 for yes and 2 for no command.")
+print("Operation: 4/10")
+answer= int(input("Response: "))
+command1 = 1
+command2 = 2
+if answer == command2:
+    print("Okay, then rest.")
+    raise SystemExit
+elif answer <= -1:
+    print("Input the right command.")
+    raise SystemExit
+elif answer == command1:
+    print("Continue to the survey")
+elif answer >= command2:
+    print("Input the right command.")
+    raise SystemExit
+
+print("Set#5 of random numbers")
+num5 = ran_num9 + ran_num10 
+print("The answer in operation#5 is:",num5)
+print("Random number1 is: ",ran_num9)
+print("Random number2 is: ",ran_num10)
+print("Evaluating if the answer is correct: Type 1 for yes and 2 for no command.")
+print("Operation: 5/10")
+answer= int(input("Response: "))
+command1 = 1
+command2 = 2
+if answer == command2:
+    print("Okay, then rest.")
+    raise SystemExit
+elif answer <= -1:
+    print("Input the right command.")
+    raise SystemExit
+elif answer == command1:
+    print("Continue to the survey")
+elif answer >= command2:
+    print("Input the right command.")
+    raise SystemExit
+
+print("Set#6 of random numbers")
+num6 = ran_num11 + ran_num12 
+print("The answer in operation#6 is:",num6)
+print("Random number1 is: ",ran_num11)
+print("Random number2 is: ",ran_num12)
+print("Evaluating if the answer is correct: Type 1 for yes and 2 for no command.")
+print("Operation: 6/10")
+answer= int(input("Response: "))
+command1 = 1
+command2 = 2
+if answer == command2:
+    print("Okay, then rest.")
+    raise SystemExit
+elif answer <= -1:
+    print("Input the right command.")
+    raise SystemExit
+elif answer == command1:
+    print("Continue to the survey")
+elif answer >= command2:
+    print("Input the right command.")
+    raise SystemExit
+
+print("Set#7 of random numbers")
+num7 = ran_num13 + ran_num14 
+print("The answer in operation#7 is:",num7)
+print("Random number1 is: ",ran_num13)
+print("Random number2 is: ",ran_num14)
+print("Evaluating if the answer is correct: Type 1 for yes and 2 for no command.")
+print("Operation: 7/10")
+answer= int(input("Response: "))
+command1 = 1
+command2 = 2
+if answer == command2:
+    print("Okay, then rest.")
+    raise SystemExit
+elif answer <= -1:
+    print("Input the right command.")
+    raise SystemExit
+elif answer == command1:
+    print("Continue to the survey")
+elif answer >= command2:
+    print("Input the right command.")
+    raise SystemExit
+
+print("Set#8 of random numbers")
+num8 = ran_num15 + ran_num16
+print("The answer in operation#8 is:",num8)
+print("Random number1 is: ",ran_num15)
+print("Random number2 is: ",ran_num16)
+print("Evaluating if the answer is correct: Type 1 for yes and 2 for no command.")
+print("Operation: 8/10")
+answer= int(input("Response: "))
+command1 = 1
+command2 = 2
+if answer == command2:
+    print("Okay, then rest.")
+    raise SystemExit
+elif answer <= -1:
+    print("Input the right command.")
+    raise SystemExit
+elif answer == command1:
+    print("Continue to the survey")
+elif answer >= command2:
+    print("Input the right command.")
+    raise SystemExit
+print("Set#9 of random numbers")
+num9 = ran_num17 + ran_num18 
+print("The answer in operation#9 is:",num9)
+print("Random number1 is: ",ran_num17)
+print("Random number2 is: ",ran_num18)
+print("Evaluating if the answer is correct: Type 1 for yes and 2 for no command.")
+print("Operation: 9/10")
+answer= int(input("Response: "))
+command1 = 1
+command2 = 2
+if answer == command2:
+    print("Okay, then rest.")
+    raise SystemExit
+elif answer <= -1:
+    print("Input the right command.")
+    raise SystemExit
+elif answer == command1:
+    print("Continue to the survey")
+elif answer >= command2:
+    print("Input the right command.")
+    raise SystemExit
+
+print("Set#10 of random numbers")
+num10 = ran_num19 + ran_num20 
+print("The answer in operation#10 is:",num10)
+print("Random number1 is: ",ran_num19)
+print("Random number2 is: ",ran_num20)
+print("Evaluating if the answer is correct: Type 1 for yes and 2 for no command.")
+print("Operation: 10/10")
+answer= int(input("Response: "))
+command1 = 1
+command2 = 2
+if answer == command2:
+    print("Okay, then rest.")
+    raise SystemExit
+elif answer <= -1:
+    print("Input the right command.")
+    raise SystemExit
+elif answer == command1:
+    print("Continue to the survey")
+elif answer >= command2:
+    print("Input the right command.")
+    raise SystemExit
+
+print("Program successfully processed")
